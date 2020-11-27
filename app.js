c41228b846d665763efd1644455893aa3e2c34ea
Vlad Chepaykyn <backdround@yandex.ru>
2020-11-27 13:33:58 +0300

    Bug reproduce

---
+++ app.js
@@ -0,0 +1,22 @@
+const {app, BrowserWindow} = require('electron')
+const path = require('path')
+const url = require('url')
+
+let window = null
+
+// Wait until the app is ready
+app.once('ready', () => {
+  // Create a new window
+  window = new BrowserWindow({
+  })
+
+  window.loadURL(url.format({
+    pathname: path.join(__dirname, 'index.html'),
+    protocol: 'file:',
+    slashes: true
+  }))
+
+  window.once('ready-to-show', () => {
+    window.webContents.openDevTools()
+  })
+})
